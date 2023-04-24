d359209e6f5c6b5c2e7f038e57c4ad85bda9fe1b
ferrreo <harderthanfire@gmail.com>
2023-04-24 19:34:51 +0100

    Update to lunar

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ dpkg-sig --sign builder ./output/*.deb
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
