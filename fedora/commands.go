879bbd9523596bc94b8aaa60ecd2c12799b74b8a
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-08-17 18:56:21 +0300

    add auto init for apx v2

--- fedora/commands.go
+++ fedora/commands.go
@@ -2,6 +2,10 @@ package fedora
 
 var PackageManager = "apx"
 
+var ContainerSubsystem = "pikman-fedora-workstation"
+
+var ApxSubsystem = "fedora-workstation"
+
 var Commands = map[string]string{
 	"autoremove": "pikman-fedora-workstation autoremove",
 	"clean":      "pikman-fedora-workstation clean",
