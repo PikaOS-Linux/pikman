f3c3c143f0defbce2260fb8ed7b4324951e55c18
ferrreo <harderthanfire@gmail.com>
2024-04-11 17:08:42 +0100

    Update commands.go

--- ubuntu/commands.go
+++ ubuntu/commands.go
@@ -1,6 +1,6 @@
 package ubuntu
 
-var PackageManager = "sudo -S apt"
+var PackageManager = "sudo -S nala"
 
 var Commands = map[string]string{
 	"autoremove": "autoremove",
@@ -12,5 +12,5 @@ var Commands = map[string]string{
 	"search":     "search",
 	"show":       "show",
 	"update":     "update",
-	"upgrade":    "update && sudo -S apt upgrade",
+	"upgrade":    "upgrade",
 }
